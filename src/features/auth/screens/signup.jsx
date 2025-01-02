cd3129456e0854c280538356916f83b281bffa90
mowaisfarid <mowaisfarid@gmail.com>
2025-01-03 00:25:01 +0500

    firebase configs

--- src/features/auth/screens/signup.jsx
+++ src/features/auth/screens/signup.jsx
@@ -1,30 +1,26 @@
 import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-// external libararies
+// Import Firestore correctly
 import firestore from '@react-native-firebase/firestore';
 
 const Signup = () => {
-  useEffect(() => {
-    const addUser = async () => {
-      console.log('this');
-      firestore()
-        .collection('Users')
-        .doc('ABC')
-        .set({
-          name: 'Ada Lovelace',
-          age: 30,
-        })
-        .then(() => {
-          console.log('User added!');
-        })
-        .catch(err => {
-          console.log('error', err);
-        });
-    };
+  const addUser = async () => {
+    console.log('Attempting to add user...');
+    try {
+      // Add a user to the Firestore collection
+      await firestore().collection('Users').doc('ABC').set({
+        name: 'Ada Lovelace',
+        age: 30,
+      });
+      console.log('User added successfully!');
+    } catch (error) {
+      console.error('Error adding user:', error);
+    }
+  };
 
+  useEffect(() => {
     addUser();
-    console.log('this runs');
   }, []);
 
   return (
